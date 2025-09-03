d96210c0f310f978f1dddd65b95f5c015847caa0
Gouravkar123 <gkar9031@gmail.com>
2025-09-03 16:19:40 +0530

    Improve Homepage Styling

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseServerError
 
 def about(request):
-    return render(request, "about.html", {"title": "About Our Restaurant"})
+    try:
+        return render(request, "about.html", {"title": "About Our Restaurant"})
+    except Exception as e:
+        # Log the error if you have logging configured
+        return HttpResponseServerError("An error occurred while loading the About page.")
